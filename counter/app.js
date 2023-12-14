951f27b0698ae3407c802e0d29462f83d6c26abb
Lael Aichaoui <lael.aichaoui@gmail.com>
2023-12-14 09:13:37 -0800

    added js functionnality

---
+++ counter/app.js
@@ -0,0 +1,30 @@
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+// set initial count
+let count = 0;
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+      value.style.color = "black";
+    }
+
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+
+    value.textContent = count;
+  });
+});
