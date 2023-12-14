adb8511fea1e50e880235c7155a170bfb74a5ed3
Lael Aichaoui <lael.aichaoui@gmail.com>
2023-12-13 16:16:43 -0800

    added js for the hex page

--- color_flipper/app.js
+++ color_flipper/app.js
@@ -1,16 +1,19 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
+// select btn and text display
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
+  // radom number
   const randomNumber = getRandomNumber();
-  console.log(randomNumber);
 
+  // display random number
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
 });
 
+// generate random number
 function getRandomNumber() {
   return Math.floor(Math.random() * colors.length);
 }
--- color_flipper/hex.js
+++ color_flipper/hex.js
@@ -1,1 +1,23 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  // variable to start color with an #
+  let hexColor = "#";
+
+  // loop to make the hex color 6 character
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+
+  // display color in the DOM
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
+
+// function to generate random number
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
