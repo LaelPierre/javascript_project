6ee17d0589df2eec4a97ab225fde48c0558534cb
Lael Aichaoui <lael.aichaoui@gmail.com>
2023-12-14 17:45:13 -0800

    added js functionnality

--- navbar/app.js
+++ navbar/app.js
@@ -1,12 +1,6 @@
-const getElement = (selector) => {
-  const el = document.querySelector(selector)
-  if (el) return el
-  throw new Error(`Please check your classes : ${selector} does not exist`)
-}
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
 
-const navToggle = getElement('.nav-toggle')
-const links = getElement('.links')
-
-navToggle.addEventListener('click', function () {
-  links.classList.toggle('show-links')
-})
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show-links");
+});
