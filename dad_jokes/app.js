d21d735fc49e16ecc083d4bb01a4b3c8a136f302
Lael Aichaoui <lael.aichaoui@gmail.com>
2024-01-02 20:05:31 -0800

    fetch data from api and display

--- dad_jokes/app.js
+++ dad_jokes/app.js
@@ -1,1 +1,31 @@
-console.log('Dad Jokes Starter');
+const url = "https://icanhazdadjoke.com/";
+
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
+
+btn.addEventListener("click", () => {
+  fetchDadJokes();
+});
+
+const fetchDadJokes = async () => {
+  result.textContent = "Loading...";
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Accept: "application/json",
+        "User-Agent": "learning app",
+      },
+    });
+    if (!response.ok) {
+      throw new Error("there was an error");
+    }
+
+    const data = await response.json();
+    result.textContent = data.joke;
+  } catch (error) {
+    result.textContent = "There was an error...";
+  }
+};
+
+fetchDadJokes();
