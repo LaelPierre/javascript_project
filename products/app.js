ac9423f696b6ec4eb59667f5a3352781bafaed06
Lael Aichaoui <lael.aichaoui@gmail.com>
2024-01-07 10:16:14 -0800

    added fetch method

--- products/app.js
+++ products/app.js
@@ -1,1 +1,22 @@
-console.log('products starter');
+const url = "https://course-api.com/javascript-store-products";
+
+const productsDOM = document.querySelector(".products-center");
+
+const fetchProducts = async () => {
+  productsDOM.innerHTML = '<div class="loading"></div>';
+  try {
+    const resp = await fetch(url);
+
+    if (!resp.ok) {
+      throw new Error("there was an error");
+    }
+
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    productsDOM.innerHTML = '<p class="error">there was an error...</p>';
+  }
+};
+
+fetchProducts();
