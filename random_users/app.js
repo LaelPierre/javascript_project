b97c7b069617338b9a38e7ffe34022524104ed7b
Lael Aichaoui <lael.aichaoui@gmail.com>
2024-01-13 17:08:19 -0800

    added display user

--- random_users/app.js
+++ random_users/app.js
@@ -1,18 +1,15 @@
 import getElement from "./utils/getElement.js";
 import getUser from "./utils/fetchUser.js";
+import displayUser from "./utils/displayUser.js";
 
-const img = getElement(".user-img");
-const title = getElement(".user-title");
-const value = getElement(".user-value");
 const btn = getElement(".btn");
-const btns = [...document.querySelectorAll(".icon")];
 
 const showUser = async () => {
   // get user from api
   const person = await getUser();
-  console.log(person);
+  displayUser(person);
   // display user
 };
 
 window.addEventListener("DOMContentLoaded", showUser);
-window.addEventListener("click", showUser);
+btn.addEventListener("click", showUser);
